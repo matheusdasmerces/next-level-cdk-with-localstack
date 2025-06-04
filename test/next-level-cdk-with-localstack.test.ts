0779bf856846a7c02fddd178f652b8f8c46829ee
matheusdasmerces <matheusdmerces@gmail.com>
2025-06-04 16:58:02 +0200

    Initial commit

---
+++ test/next-level-cdk-with-localstack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NextLevelCdkWithLocalstack from '../lib/next-level-cdk-with-localstack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/next-level-cdk-with-localstack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NextLevelCdkWithLocalstack.NextLevelCdkWithLocalstackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
